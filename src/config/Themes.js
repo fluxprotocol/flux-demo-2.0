81b37cb18bf9f85db2ab0387440764138ebd0e8b
Zish Rodrigues <zish.rodrigues@gmail.com>
2020-07-20 12:20:44 +0200

    Added theme colors

--- src/config/Themes.js
+++ src/config/Themes.js
@@ -1,8 +1,28 @@
+const categoryColors = {
+  esports: '#4C6BF5',
+  politics: '#0004FF',
+  crypto: '#5400FF',
+  startups: '#7400DA',
+  stockmarket: '#C45DFF',
+  meme: '#FF00FD',
+  viral: '#FF009C',
+  sports: '#FF1958',
+}
+
+const globalColors = {
+  purple: '#A743FF',
+  pink: '#FF009C',
+  red: '#FF1958',
+  blue: '#4C6BF5',
+}
+
 export const darkTheme = {
   body: '#2C2A43',
   text: '#FAFAFA',
   toggleBorder: '#6B8096',
   background: '#999',
+  ...categoryColors,
+  ...globalColors,
 }
 
 export const lightTheme = {
@@ -10,4 +30,6 @@ export const lightTheme = {
   text: '#fff',
   toggleBorder: '#FFF',
   background: '#363537',
+  ...categoryColors,
+  ...globalColors,
 }
