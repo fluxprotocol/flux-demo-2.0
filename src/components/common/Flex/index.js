44f6cfb78d88006bd4fc2853cfb889c1d23ef355
Ioannis Kapritsias <jkapritsias@gmail.com>
2020-07-26 11:49:10 +0200

    Added additional style prop rules to flex component for reusability inside OrderBook component

--- src/components/common/Flex/index.js
+++ src/components/common/Flex/index.js
@@ -5,11 +5,18 @@ export const FlexWrapper = styled.div`
   align-items: center;
   flex-direction: ${props => props.flexDirection ? props.flexDirection : 'initial'};
   padding: ${props => props.padding ? props.padding : 0};
+  width: ${props => props.width ? props.width : 'auto'};
 `;
 
 export const FlexItem = styled.div`
   flex:1;
   margin: ${props => props.margin ? props.margin : 0};
+  padding: ${props => props.padding ? props.padding : '0'};
   text-align: ${props => props.textAlign ? props.textAlign : 'initial'};
-  color: white;
+  color: ${props => props.color ? props.color : 'white'};
+  background: ${props => props.backgroundColor ? props.backgroundColor : 'none'};
+  max-width: ${props => props.maxWidth ? props.maxWidth : 'initial'};
+  &.active {
+    border-radius: ${props => props.borderRadius ? props.borderRadius : 'initial'};
+  }
 `;
