2e7bfeae5302f50dd51f1d88f15991080a83922b
Ioannis Kapritsias <jkapritsias@gmail.com>
2020-07-27 17:18:01 +0200

    rewritten filters to radio buttons with labels

--- src/components/common/OrderBookBarChart/index.js
+++ src/components/common/OrderBookBarChart/index.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 // common
 import { FlexWrapper, FlexItem } from '../../common/Flex';
+
+const RadioButton = styled.input.attrs({ type: 'radio' })`
+  display: none;
+
+  &:checked + label {
+    opacity: 1;
+    color: white;
+    background-color: #3B3A64;
+    border-radius: 4px
+  }
+`;
+
+const RadioLabel = styled.label`
+  display: inline-block;
+  width: auto;
+  padding: 1rem;
+  font-size: 0.8rem;
+  color: gray;
+  opacity: 0.7;
+  cursor: pointer;
+  text-align: center;
+`;
+
+const FilterLabel = styled.span`
+  display: inline-block;
+  vertical-align: middle;
+`;
 
 const PageWrapper = styled.div`
   display: flex;
@@ -19,11 +46,12 @@ const OrderBookDetails = styled.tr`
 
 const OrderBookDetail = styled.th`
   padding: .5em;
-
+  font-weight: ${props => props.fontWeight ? props.fontWeight : 'auto'}
 `;
 
 const OrderBookData = styled.td`
   padding: .8em;
+  color: ${props => props.color ? props.color : '#FF009C'};
   &.range {
     margin: 1em;
     padding: 0 .1em;
@@ -39,75 +67,190 @@ const BarWrapper = styled.span`
   display: block;
   border-radius: 12px 0 0 12px;
   padding: 3px 4px 3px 0;
-  background-color: ${props => props.backgroundColor ? props.backgroundColor : 'red'}
+  color: white;
+  background-color: ${props => props.backgroundColor ? props.backgroundColor : '#FF009C'};
 `
 
 const OrderBookBookBarChart = props => {
+  // define default state
+  const [checked, setChecked] = useState('all');
+  
+  // handler for filter selections
+  const handleRadioChange = (event) => {
+    setChecked(event.target.value);
+    let filterValue = event.target.value;
+    console.log('make request for maket detail with filter value', filterValue)
+  }
+
   return (
     <PageWrapper>
         <FlexWrapper
           width="80%"
         >
-          <FlexItem 
-            color="gray" 
-            textAlign="center"
-          >
-            1D
-          </FlexItem>
-          <FlexItem 
-            color="gray" 
-            textAlign="center"
-          >
-            1W
-          </FlexItem>
-          <FlexItem 
-            color="gray" 
-            textAlign="center"
-          >
-            1M
-          </FlexItem>
-          <FlexItem 
-            color="gray" 
-            textAlign="center"
-          >
+          <RadioButton 
+            name="filterValue" 
+            id="1D"
+            value="1D"
+            checked={checked === '1D'}
+            onChange={handleRadioChange}
+          />
+          <RadioLabel htmlFor="1D">
+            <FilterLabel>
+              1D
+            </FilterLabel>
+          </RadioLabel>
+
+          <RadioButton 
+            name="filterValue" 
+            id="1W"
+            value="1W"
+            checked={checked === '1W'}
+            onChange={handleRadioChange}
+          />
+          <RadioLabel htmlFor="1W">
+            <FilterLabel>
+              1W
+            </FilterLabel>
+          </RadioLabel>
+
+          <RadioButton 
+            name="filterValue" 
+            id="1M"
+            value="1M"
+            checked={checked === '1M'}
+            onChange={handleRadioChange}
+          />
+          <RadioLabel htmlFor="1M">
+            <FilterLabel>
+              1M
+            </FilterLabel>
+          </RadioLabel>
+
+        <RadioButton 
+          name="filterValue" 
+          id="3M"
+          value="3M"
+          checked={checked === '3M'}
+          onChange={handleRadioChange}
+        />
+        <RadioLabel htmlFor="3M">
+          <FilterLabel>
             3M
-          </FlexItem>
-          <FlexItem 
-            color="gray" 
-            textAlign="center"
-          >
-            6M
-          </FlexItem>
-          <FlexItem 
-            color="gray" 
-            textAlign="center"
-          >
+          </FilterLabel>
+        </RadioLabel>
+
+        <RadioButton 
+          name="filterValue" 
+          id="6M"
+          value="6M"
+          checked={checked === '6M'}
+          onChange={handleRadioChange}
+        />
+        <RadioLabel htmlFor="6M">
+          <FilterLabel>
+            6W
+          </FilterLabel>
+        </RadioLabel>
+
+        <RadioButton 
+          name="filterValue" 
+          id="1Y"
+          value="1Y"
+          checked={checked === '1Y'}
+          onChange={handleRadioChange}
+        />
+        <RadioLabel htmlFor="1Y">
+          <FilterLabel>
             1Y
-          </FlexItem>
-          {/* should become dynamic active class */}
-          <FlexItem className="active"
-            color="gray" 
-            textAlign="center"
-            padding="13px"
-            backgroundColor="#3B3A64"
-            borderRadius="4px"
-            maxWidth="4em"
-          >
-            ALL
-          </FlexItem>
+          </FilterLabel>
+        </RadioLabel>
+
+        <RadioButton 
+          name="filterValue" 
+          id="all"
+          value="all"
+          checked={checked === 'all'}
+          onChange={handleRadioChange}
+        />
+        <RadioLabel htmlFor="all">
+          <FilterLabel>
+            all
+          </FilterLabel>
+        </RadioLabel>
         </FlexWrapper>
 
         <FlexWrapper>
           <OrderBookWrapper>
             <colgroup span="4"></colgroup>
             <OrderBookDetails>
-              <OrderBookDetail>Contract</OrderBookDetail>
-              <OrderBookDetail>Amount</OrderBookDetail>
-              <OrderBookDetail>Price</OrderBookDetail>
-              <OrderBookDetail>Type</OrderBookDetail>
+              <OrderBookDetail
+                fontWeight="100"
+              >Contract</OrderBookDetail>
+              <OrderBookDetail
+                fontWeight="100"
+              >Amount</OrderBookDetail>
+              <OrderBookDetail
+                fontWeight="100"
+              >Price</OrderBookDetail>
+              <OrderBookDetail
+                fontWeight="100"
+              >Type</OrderBookDetail>
             </OrderBookDetails>
             <OrderBookDetails>
-              <OrderBookData>Trump</OrderBookData>
+              <OrderBookData
+                color="white"
+              >Trump</OrderBookData>
+              <OrderBookData 
+                className="range"
+                borderRadius="4px"
+                minWidth="10em"
+              >
+                <BarWrapper>
+                  1200
+                </BarWrapper>
+              </OrderBookData>
+              <OrderBookData>
+                0.40
+              </OrderBookData>
+              <OrderBookData>
+                sell
+              </OrderBookData>
+            </OrderBookDetails>
+            <OrderBookDetails>
+              <OrderBookData
+                color="white"
+              >
+                Biden
+              </OrderBookData>
+              <OrderBookData 
+                className="range"
+                borderRadius="4px"
+                minWidth="10em"
+              >
+                <BarWrapper
+                  backgroundColor="#C4FF88"
+                  color="white"
+                >
+                  1200
+                </BarWrapper>
+              </OrderBookData>
+              <OrderBookData
+                color="#C4FF88"
+              >
+                0.40
+              </OrderBookData>
+              <OrderBookData
+                color="#C4FF88"
+              >
+                buy
+              </OrderBookData>
+            </OrderBookDetails>
+            <OrderBookDetails>
+              <OrderBookData
+                color="white"
+              >
+                Jon
+              </OrderBookData>
               <OrderBookData 
                 className="range"
                 borderRadius="4px"
@@ -121,46 +264,33 @@ const OrderBookBookBarChart = props => {
               <OrderBookData>sell</OrderBookData>
             </OrderBookDetails>
             <OrderBookDetails>
-              <OrderBookData>Biden</OrderBookData>
+              <OrderBookData
+                color="white"
+              >
+                Other
+              </OrderBookData>
               <OrderBookData 
                 className="range"
                 borderRadius="4px"
                 minWidth="10em"
               >
-                <BarWrapper>
+                <BarWrapper
+                  backgroundColor="#C4FF88"
+                  color="white"
+                >
                   1200
                 </BarWrapper>
               </OrderBookData>
-              <OrderBookData>0.40</OrderBookData>
-              <OrderBookData>buy</OrderBookData>
-            </OrderBookDetails>
-            <OrderBookDetails>
-              <OrderBookData>Jon</OrderBookData>
-              <OrderBookData 
-                className="range"
-                borderRadius="4px"
-                minWidth="10em"
+              <OrderBookData
+                color="#C4FF88"
               >
-                <BarWrapper>
-                  1200
-                </BarWrapper>
+                0.40
               </OrderBookData>
-              <OrderBookData>0.40</OrderBookData>
-              <OrderBookData>sell</OrderBookData>
-            </OrderBookDetails>
-            <OrderBookDetails>
-              <OrderBookData>Other</OrderBookData>
-              <OrderBookData 
-                className="range"
-                borderRadius="4px"
-                minWidth="10em"
+              <OrderBookData
+                color="#C4FF88"
               >
-                <BarWrapper>
-                  1200
-                </BarWrapper>
+                buy
               </OrderBookData>
-              <OrderBookData>0.40</OrderBookData>
-              <OrderBookData>buy</OrderBookData>
             </OrderBookDetails>
           </OrderBookWrapper>
         </FlexWrapper>
