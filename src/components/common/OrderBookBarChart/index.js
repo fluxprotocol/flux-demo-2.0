e04fb4305aeb59a93995c583e088e781a711bb25
Ioannis Kapritsias <jkapritsias@gmail.com>
2020-08-05 12:09:24 +0200

    made orderbook and orderbooklinegraph modular, rendering these components based on the active state of the chartview buttons inside the marketdetaildata module

--- src/components/common/OrderBookBarChart/index.js
+++ src/components/common/OrderBookBarChart/index.js
@@ -1,21 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const positiveArrow = require('../../../assets/images/icons/green_arrow.svg');
-// const negativeArrow = require('../../../assets/images/icons/pink_arrow.svg');
-
-const barsActive = require('../../../assets/images/icons/bars_active.svg');
-const barsInactive = require('../../../assets/images/icons/bars_inactive.svg');
-const graphActive = require('../../../assets/images/icons/graph_active.svg');
-const graphInactive = require('../../../assets/images/icons/graph_inactive.svg');
-
-const ChartWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  align-items: start;
-`;
-
 const FlexWrapper = styled.div`
   display: flex;
   flex-direction: row;
@@ -24,113 +9,6 @@ const FlexWrapper = styled.div`
   margin-bottom: 1em;
   width: 90%;
 `;
-
-const OverviewWrapper = styled.div`
-  width: 90%;
-  margin: 0 auto;
-  margin-top: 2em;
-  margin-bottom: 1.25em;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-const ShareDetails = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const DetailPriceHeading = styled.div`
-  display: flex;
-  align-items: baseline;
-`;
-
-const DetailPriceLabel = styled.div`
-  font-weight: 900;
-  font-size: 1.5em;
-  margin-right: .5em;
-`;
-const DetailStatHeading = styled.div`
-  display: flex;
-`;
-
-const DetailStatLabel = styled.div`
-  color: ${props => props.color ? props.color : '#C4FF88' };
-  margin-right: .5em;
-`;
-
-const FormatContainer = styled.div`
-  display: flex;
-  align-items: flex-start;
-
-  & > img {
-    margin: 0 .25em;
-  }
-`;
-
-const FilterWrapper = styled.div`
-  display: inline-block;
-  margin-bottom: 0.5rem;
-  width: 50%;
-  text-align: center;
-
-  @media (min-width: ${({ theme }) => theme.mediumBreakpoint}) {
-    width: auto;
-  }
-`;
-
-const ChartButton = styled.input.attrs({ type: 'radio' })`
-  display: none;
-
-  &:checked + label {
-    opacity: 1;
-    ${props => props.theme.text}
-  }
-`;
-
-const ChartLabel = styled.label`
-  display: inline-block;
-  width: 100%;
-  padding: 0.4rem 0 0.4rem .75rem;
-  font-size: 0.8rem;
-  color: ${props => props.theme.text};
-  opacity: 0.7;
-  cursor: pointer;
-  user-select: none;
-  transition: all 0.1s ease;
-`;
-
-const RadioButton = styled.input.attrs({ type: 'radio' })`
-  display: none;
-
-  &:checked + label {
-    opacity: 1;
-    color: white;
-    background-color: #3B3A64;
-    border-radius: 4px
-  }
-`;
-
-const RadioLabel = styled.label`
-  display: inline-block;
-  width: auto;
-  padding: 1rem;
-  font-size: 0.8rem;
-  color: gray;
-  opacity: 0.7;
-  cursor: pointer;
-  text-align: center;
-`;
-
-const FilterLabel = styled.span`
-  display: inline-block;
-  vertical-align: middle;
-`;
-
-const PageWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`
 
 const OrderBookWrapper = styled.table`
   width: 100%;
@@ -188,203 +66,75 @@ const BarWrapperContainer = styled.div`
   }
 ` 
 
-// pass width: 0 / 100 fill width dynamiccaly 
-const BarWrapper = styled.span`
-  display: block;
-  position: relative;
-  padding: 3px 4px 3px 0;
-  color: white;
-  width: 100%;
-`;
-
 const OrderBookBookBarChart = props => {
-  // define default state
-  const [checked, setChecked] = useState('all');
-  const [orderBookItems, setOrderBookItems] = useState([
-    {
-      contract: 'Trump',
-      amount: '1200',
-      price: '0.40',
-      type: 'buy',
-    },
-    {
-      contract: 'Biden',
-      amount: '1000',
-      price: '0.30',
-      type: 'sell',
-    },
-    {
-      contract: 'Kanye',
-      amount: '1200',
-      price: '0.40',
-      type: 'sell',
-    },
-    {
-      contract: 'Tom',
-      amount: '1200',
-      price: '0.40',
-      type: 'sell',
-    },
-  ]);
-  const orderBookFilters = [
-    '1D',
-    '1W',
-    '1M',
-    '3M',
-    '6W',
-    'all',
-  ];
-  const orderBookHeaders = [
-    'Contact',
-    'Amount',
-    'Price',
-    'Type',
-  ];
+  
 
   const colorValue = {
     sell: 'pink',
     buy: 'green'
   };
-
-  let barIcon = barsActive;
-  let chartIcon = graphInactive;
   
   // handler for filter selections
   const handleRadioChange = (event) => {
-    setChecked(event.target.value);
     let filterValue = event.target.value;
-    console.log(filterValue);
-    barIcon = barsInactive;
-    chartIcon = graphActive;
+    console.log('this is the filtervalue', filterValue);
   }
 
+  const {orderBookHeaders, orderBookItems } = props;
+
   return (
-    <PageWrapper>
-        <OverviewWrapper>
-          <ShareDetails>
-            <DetailPriceHeading>
-              <DetailPriceLabel>
-                $0.75
-              </DetailPriceLabel>
-              per Yes share
-            </DetailPriceHeading>
-            <DetailStatHeading>
-              <DetailStatLabel>
-                <img alt="positiveArrow" src={positiveArrow} /> $0.03 (%)
-              </DetailStatLabel>
-              Past week
-            </DetailStatHeading>
-          </ShareDetails>
-          <FormatContainer>
-            <ChartWrapper>
-              <FilterWrapper>
-                <ChartButton 
-                  name="overviewType" 
-                  id="graph"
-                  value="graph"
-                  checked={checked === 'graph'}
-                  onChange={handleRadioChange}
-                />
-                <ChartLabel htmlFor="graph">
-                  <FilterLabel>
-                    <img alt="graphIcon" src={chartIcon} />
-                  </FilterLabel>
-                </ChartLabel>
-              </FilterWrapper>
+    <FlexWrapper>
+      <OrderBookWrapper>
+        <tbody>
+          <OrderBookDetails>
 
-              <FilterWrapper>
-                <ChartButton 
-                  name="overviewType" 
-                  id="bars"
-                  value="bars"
-                  checked={checked === 'bars'}
-                  onChange={handleRadioChange}
-                />
-                <ChartLabel htmlFor="bars">
-                  <FilterLabel>
-                    <img alt="barIcon" src={barIcon} />
-                  </FilterLabel>
-                </ChartLabel>
-              </FilterWrapper>
-            </ChartWrapper>
-          </FormatContainer>
-        </OverviewWrapper>
-        <FlexWrapper
-          width="100%"
-        >
+          {orderBookHeaders.map((orderBookHeader, index) => (
+            <OrderBookDetail
+              fontWeight="100"
+              key={orderBookHeader}
+            >
+              {orderBookHeader}
+            </OrderBookDetail>
+          ))}
 
-        {orderBookFilters.map((filter, index) => (
-          <div key={filter}>
-            <RadioButton 
-              name="filterValue" 
-              id={filter}
-              value={filter}
-              checked={checked === filter}
-              onChange={handleRadioChange}
-            />
-            <RadioLabel htmlFor={filter}>
-              <FilterLabel>
-                {filter}
-              </FilterLabel>
-            </RadioLabel>
-          </div>
-        ))}
+          </OrderBookDetails>
 
-        </FlexWrapper>
-
-        <FlexWrapper>
-          <OrderBookWrapper>
-            <tbody>
-              <OrderBookDetails>
-
-              {orderBookHeaders.map((orderBookHeader, index) => (
-                <OrderBookDetail
-                  fontWeight="100"
-                  key={orderBookHeader}
+          {orderBookItems.map((orderBookItem, index) => (
+            <OrderBookDetails key={orderBookItem.contract}>
+              <OrderBookData
+                color="white"
+              >{orderBookItem.contract}</OrderBookData>
+              <OrderBookData 
+                className="range"
+                borderRadius="4px"
+                width="100%"
+                backgroundColor={colorValue[orderBookItem.type]}
+              >
+                <BarWrapperContainer
+                  color={colorValue[orderBookItem.type]}
+                  backgroundColor={colorValue[orderBookItem.type]}
+                  width={orderBookItem.price * 100}
+                  content={orderBookItem.amount}
                 >
-                  {orderBookHeader}
-                </OrderBookDetail>
-              ))}
+                </BarWrapperContainer>
+              </OrderBookData>
+              <OrderBookData
+                color={colorValue[orderBookItem.type]}
+                
+              >
+                {orderBookItem.price}
+              </OrderBookData>
+              <OrderBookData
+                color={colorValue[orderBookItem.type]}
+              >
+                {orderBookItem.type}
+              </OrderBookData>
+            </OrderBookDetails>
+          ))}
 
-              </OrderBookDetails>
-
-              {orderBookItems.map((orderBookItem, index) => (
-                <OrderBookDetails key={orderBookItem.contract}>
-                  <OrderBookData
-                    color="white"
-                  >{orderBookItem.contract}</OrderBookData>
-                  <OrderBookData 
-                    className="range"
-                    borderRadius="4px"
-                    width="100%"
-                    backgroundColor={colorValue[orderBookItem.type]}
-                  >
-                    <BarWrapperContainer
-                      color={colorValue[orderBookItem.type]}
-                      backgroundColor={colorValue[orderBookItem.type]}
-                      width={orderBookItem.price * 100}
-                      content={orderBookItem.amount}
-                    >
-                    </BarWrapperContainer>
-                  </OrderBookData>
-                  <OrderBookData
-                    color={colorValue[orderBookItem.type]}
-                    
-                  >
-                    {orderBookItem.price}
-                  </OrderBookData>
-                  <OrderBookData
-                    color={colorValue[orderBookItem.type]}
-                  >
-                    {orderBookItem.type}
-                  </OrderBookData>
-                </OrderBookDetails>
-              ))}
- 
-            </tbody>
-          </OrderBookWrapper>
-        </FlexWrapper>        
-    </PageWrapper>
+        </tbody>
+      </OrderBookWrapper>
+    </FlexWrapper>        
   );
 }
 
---
+++ src/components/common/OrderBookLineGraph/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const FlexWrapper = styled.div`
+
+`;
+
+
+
+const OrderBookLineChart = props => {
+
+
+  return (
+    <FlexWrapper>
+      <h2>This should become the line chart..</h2>
+    </FlexWrapper>        
+  );
+}
+
+export default OrderBookLineChart;
